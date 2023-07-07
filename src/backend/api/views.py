93bf0f70c9b9d72fb45ad279631d5b52e9feef14
schuggermi <mschuchowski@gmx.de>
2023-07-07 19:45:35 +0200

    initial commit

---
+++ src/backend/api/views.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from django.http import JsonResponse
+
+
+def main(request):
+    current_time = datetime.now().strftime("%-I:%S %p")
+    current_date = datetime.now().strftime("%A %m %-Y")
+
+    data = {
+        'time': current_time,
+        'date': current_date,
+    }
+
+    return JsonResponse(data)
